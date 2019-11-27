056a69f78d6f83108afefc5073b8f6b7235e9bee
lemon.liao <lemon.liao@oneplus.net>
2019-11-27 16:53:02 +0800

    kv

--- js/canvas-particle.js
+++ js/canvas-particle.js
@@ -1,20 +1,104 @@
-var CanvasParticle=(function(){function getElementByTag(name){return document.getElementsByTagName(name);}
-function getELementById(id){return document.getElementById(id);}
-function canvasInit(canvasConfig){canvasConfig=canvasConfig||{};var html=getElementByTag("html")[0];var body=document.getElementById("mydiv");var canvasObj=document.createElement("canvas");var canvas={element:canvasObj,points:[],config:{vx:canvasConfig.vx||4,vy:canvasConfig.vy||4,height:canvasConfig.height||2,width:canvasConfig.width||2,count:canvasConfig.count||100,color:canvasConfig.color||"121, 162, 185",stroke:canvasConfig.stroke||"130,255,255",dist:canvasConfig.dist||6000,e_dist:canvasConfig.e_dist||20000,max_conn:10}};if(canvas.element.getContext("2d")){canvas.context=canvas.element.getContext("2d");}else{return null;}
-body.style.padding="0";body.style.margin="0";body.appendChild(canvas.element);canvas.element.style="position: relative; top: 0; left: 0; z-index: -1;";canvasSize(canvas.element);window.onresize=function(){canvasSize(canvas.element);}
-body.onmousemove=function(e){var event=e||window.event;canvas.mouse={x:event.clientX,y:event.clientY}}
-document.onmouseleave=function(){canvas.mouse=undefined;}
-setInterval(function(){drawPoint(canvas);},40);}
-function canvasSize(canvas){var width=document.getElementById("mydiv").style.width;var height=document.getElementById("mydiv").style.height;width=parseInt(width);height=parseInt(height);canvas.width=width||window.innerWeight||document.documentElement.clientWidth||document.body.clientWidth;canvas.height=height||window.innerWeight||document.documentElement.clientHeight||document.body.clientHeight;}
-function drawPoint(canvas){var context=canvas.context,point,dist;context.clearRect(0,0,canvas.element.width,canvas.element.height);context.beginPath();context.fillStyle="rgb("+canvas.config.color+")";for(var i=0,len=canvas.config.count;i<len;i++){if(canvas.points.length!=canvas.config.count){point={x:Math.floor(Math.random()*canvas.element.width),y:Math.floor(Math.random()*canvas.element.height),vx:canvas.config.vx/2-Math.random()*canvas.config.vx,vy:canvas.config.vy/2-Math.random()*canvas.config.vy}}else{point=borderPoint(canvas.points[i],canvas);}
-context.fillRect(point.x-canvas.config.width/2,point.y-canvas.config.height/2,canvas.config.width,canvas.config.height);canvas.points[i]=point;}
-drawLine(context,canvas,canvas.mouse);context.closePath();}
-function borderPoint(point,canvas){var p=point;if(point.x<=0||point.x>=canvas.element.width){p.vx=-p.vx;p.x+=p.vx;}else if(point.y<=0||point.y>=canvas.element.height){p.vy=-p.vy;p.y+=p.vy;}else{p={x:p.x+p.vx,y:p.y+p.vy,vx:p.vx,vy:p.vy}}
-return p;}
-function drawLine(context,canvas,mouse){context=context||canvas.context;for(var i=0,len=canvas.config.count;i<len;i++){canvas.points[i].max_conn=0;for(var j=0;j<len;j++){if(i!=j){dist=Math.round(canvas.points[i].x-canvas.points[j].x)*Math.round(canvas.points[i].x-canvas.points[j].x)+
-Math.round(canvas.points[i].y-canvas.points[j].y)*Math.round(canvas.points[i].y-canvas.points[j].y);if(dist<=canvas.config.dist&&canvas.points[i].max_conn<canvas.config.max_conn){canvas.points[i].max_conn++;context.lineWidth=0.5-dist/canvas.config.dist;context.strokeStyle="rgba("+canvas.config.stroke+","+(1-dist/canvas.config.dist)+")"
-context.beginPath();context.moveTo(canvas.points[i].x,canvas.points[i].y);context.lineTo(canvas.points[j].x,canvas.points[j].y);context.stroke();}}}
-if(mouse){dist=Math.round(canvas.points[i].x-mouse.x)*Math.round(canvas.points[i].x-mouse.x)+
-Math.round(canvas.points[i].y-mouse.y)*Math.round(canvas.points[i].y-mouse.y);if(dist>canvas.config.dist&&dist<=canvas.config.e_dist){canvas.points[i].x=canvas.points[i].x+(mouse.x-canvas.points[i].x)/20;canvas.points[i].y=canvas.points[i].y+(mouse.y-canvas.points[i].y)/20;}
-if(dist<=canvas.config.e_dist){context.lineWidth=1;context.strokeStyle="rgba("+canvas.config.stroke+","+(1-dist/canvas.config.e_dist)+")";context.beginPath();context.moveTo(canvas.points[i].x,canvas.points[i].y);context.lineTo(mouse.x,mouse.y);context.stroke();}}}}
-return canvasInit;})();
+//宇宙特效
+"use strict";
+var canvas = document.getElementById('canvas'),
+  ctx = canvas.getContext('2d'),
+  w = canvas.width = window.innerWidth,
+  h = canvas.height = window.innerHeight,
+
+  hue = 217,
+  stars = [],
+  count = 0,
+  maxStars = 200;//星星数量
+
+
+var canvas2 = document.createElement('canvas'),
+  ctx2 = canvas2.getContext('2d');
+canvas2.width = 100;
+canvas2.height = 100;
+var half = canvas2.width / 2,
+  gradient2 = ctx2.createRadialGradient(half, half, 0, half, half, half);
+gradient2.addColorStop(0.025, '#CCC');
+gradient2.addColorStop(0.1, 'hsl(' + hue + ', 61%, 33%)');
+gradient2.addColorStop(0.25, 'hsl(' + hue + ', 64%, 6%)');
+gradient2.addColorStop(1, 'transparent');
+
+ctx2.fillStyle = gradient2;
+ctx2.beginPath();
+ctx2.arc(half, half, half, 0, Math.PI * 2);
+ctx2.fill();
+
+// End cache
+
+function random(min, max) {
+  if (arguments.length < 2) {
+    max = min;
+    min = 0;
+  }
+
+  if (min > max) {
+    var hold = max;
+    max = min;
+    min = hold;
+  }
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function maxOrbit(x, y) {
+  var max = Math.max(x, y),
+    diameter = Math.round(Math.sqrt(max * max + max * max));
+  return diameter / 2;
+  //星星移动范围，值越大范围越小，
+}
+
+var Star = function() {
+
+  this.orbitRadius = random(maxOrbit(w, h));
+  this.radius = random(60, this.orbitRadius) / 8;
+  //星星大小
+  this.orbitX = w / 2;
+  this.orbitY = h / 2;
+  this.timePassed = random(0, maxStars);
+  this.speed = random(this.orbitRadius) / 50000;
+  //星星移动速度
+  this.alpha = random(2, 10) / 10;
+
+  count++;
+  stars[count] = this;
+}
+
+Star.prototype.draw = function() {
+  var x = Math.sin(this.timePassed) * this.orbitRadius + this.orbitX,
+    y = Math.cos(this.timePassed) * this.orbitRadius + this.orbitY,
+    twinkle = random(10);
+
+  if (twinkle === 1 && this.alpha > 0) {
+    this.alpha -= 0.05;
+  } else if (twinkle === 2 && this.alpha < 1) {
+    this.alpha += 0.05;
+  }
+
+  ctx.globalAlpha = this.alpha;
+  ctx.drawImage(canvas2, x - this.radius / 2, y - this.radius / 2, this.radius, this.radius);
+  this.timePassed += this.speed;
+}
+
+for (var i = 0; i < maxStars; i++) {
+  new Star();
+}
+
+function animation() {
+  ctx.globalCompositeOperation = 'source-over';
+  ctx.globalAlpha = 0.4; //尾巴
+  ctx.fillStyle = 'hsla(' + hue + ', 64%, 6%, 2)';
+  ctx.fillRect(0, 0, w, h)
+
+  ctx.globalCompositeOperation = 'lighter';
+  for (var i = 1, l = stars.length; i < l; i++) {
+    stars[i].draw();
+  };
+
+  window.requestAnimationFrame(animation);
+}
+
+animation();
--- js/homepage.js
+++ js/homepage.js
@@ -3,23 +3,6 @@
  */
 window.onload = function() {
   var isMobile = window.matchMedia && window.matchMedia('(max-width: 735px)').matches
-  //配置
-  var config = {
-    vx: 4,	//小球x轴速度,正为右，负为左
-    vy: 4,	//小球y轴速度
-    height: 2,	//小球高宽，其实为正方形，所以不宜太大
-    width: 2,
-    count: isMobile ? 50 : 200,		//点个数
-    color: "255, 255, 255", 	//点颜色
-    stroke: "220, 220, 220", 		//线条颜色
-    dist: 6000, 	//点吸附距离
-    e_dist: 20000, 	//鼠标吸附加速距离
-    max_conn: 10 	//点到点最大连接数
-  }
-
-  //调用
-  CanvasParticle(config);
-
   var docHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
   var docWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
   var controller = new ScrollMagic.Controller();
